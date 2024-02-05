1270e1a959725f68ae107a6fa295b8ad28550a71
telewizoor <mz.przemo@gmail.com>
2024-02-05 22:40:15 +0100

    Uploading files...

---
+++ start.sh
@@ -0,0 +1,4 @@
+rm /home/xxx/remoteUniden/debug.log
+rm -r /home/xxx/remoteUniden/rec
+mkdir /home/xxx/remoteUniden/rec
+node /home/xxx/remoteUniden/remoteUnidenServer.js > /home/xxx/remoteUniden/debug.log
